84848d5e996ff9f74ae07af48b46f6212309e5e9
Brian Otieno <brianokola@gmail.com>
2025-01-07 20:02:01 +0300

    Setup fee Components

--- finance_management/models.py
+++ finance_management/models.py
@@ -9,16 +9,44 @@ from django.utils.translation import gettext_lazy as _
 
 
 class Expense(models.Model):
-    description = models.TextField(_(""))
-    amount = models.DecimalField(_(""), max_digits=5, decimal_places=2)
-    date = models.DateTimeField(_(""), auto_now=True, auto_now_add=True)
+    description = models.TextField(_("description"))
+    amount = models.DecimalField(_("amount"), max_digits=5, decimal_places=2)
+    date = models.DateTimeField(_("date"), auto_now=True, auto_now_add=True)
 
     class Meta:
-        verbose_name = _("")
-        verbose_name_plural = _("s")
+        verbose_name = _("Expense")
+        verbose_name_plural = _("Expenses")
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
-        return reverse("_detail", kwargs={"pk": self.pk})
+        return reverse("Expense_detail", kwargs={"pk": self.pk})
+
+
+class FeeComponent(models.Model):
+    name = models.CharField(_("name"), max_length=50)
+    description = models.TextField(_("description"))
+
+    class Meta:
+        verbose_name = _("FeeComponent")
+        verbose_name_plural = _("FeeComponents")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("FeeComponent_detail", kwargs={"pk": self.pk})
+
+
+class FeeStructure(models.Model):
+
+    class Meta:
+        verbose_name = _("FeeStructure")
+        verbose_name_plural = _("FeeStructures")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("FeeStructure_detail", kwargs={"pk": self.pk})
